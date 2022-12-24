abfb5c3b09af28eb2ba8c2064415330f8b9f8298
mastikhor333 <patelsoham532000@gmail.com>
2022-12-24 15:15:37 +0530

    gitlab action initialize

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         <b><i> Welcome to docker workspace.</i></b>
         </p>
         <a
           className="App-link"
